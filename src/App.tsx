0a2554fa15e6a5f255c8992f9e1031c80e4ac366
Mercy-line <mercylinenyaboke08@gmail.com>
2026-04-13 10:37:58 +0300

    add app.tsx file

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import CanvasImage from "./CanvasImage";
+
+export default function App() {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>PNG Drawing Assignment</h1>
+      <CanvasImage />
+    </div>
+  );
+}
